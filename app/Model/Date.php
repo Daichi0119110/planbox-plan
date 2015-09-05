54c708ae02ab0232b665d06d56291aef6ec35596
Kazushi Kondo <kk1719365@gmail.com>
2015-09-05 14:53:10 +0900

    Make Find

--- app/Model/Date.php
+++ app/Model/Date.php
@@ -1,5 +1,22 @@
 <?php 
 
 class Date extends AppModel {
-	
+	public $name='Date';
+	public $useTable='dates';
+
+	function getdates($id){
+	$status=array(
+			'conditions'=>
+			array('id'=>$id)
+		);
+	return $this->find('first',$status);
+	}
+
+	function getdatesfromcouple($Couple_id){
+		$status=array(
+			'conditions'=>
+			array('couple_id'=>$Couple_id)
+		);
+		return $this->find('all',$status);
+	}
 }
