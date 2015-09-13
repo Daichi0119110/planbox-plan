8bf38a9718ddfd18137df623ecbdcf35ee401ac1
kazuCF <memegumama@gmail.com>
2015-09-13 14:14:02 +0900

    database.php CouplesController UserController

--- app/Controller/CouplesController.php
+++ app/Controller/CouplesController.php
@@ -2,17 +2,49 @@
 
 class CouplesController extends AppController {
 	public $helper = array('HTML', 'form');
-
-	public function couple() {
-		$this->set('couples', $this->Couple->find('all'));
-
+	public $uses=array('Couple','Date');
+	public function couple($id) {
+		
+		//$this->set('couples', $this->Couple->find('all'));
+		$this->set('couples',$this->Couple->getcouple($id));
+		$this->set('mydate',$this->Date->getdatesfromcouple($id));//ここから記事の投稿数もとれる？
 	}
 
-	public function edit() {
+	public function edit($id) {
+		//フォームとして送られてくることを想定
+		$this->Couple->id=$id;
+		if($this->request->is('post')||$this->request->is('put')){
+			$this->Couple->save($this->request->data);
+	//		$this->Couple->redirect(array('action'=>'Couple'));
+		}else{
+			$this->request->data=$this->Couple->read(null,$id);
+		}
+	}
 
+	public function delete($id){
+	$this->Date->deleateAll(array('Date.couple_id' => $id));	//投稿している記事も削除
+	$this->Couple->deleate($id,false);//ユーザ情報は削除しない
 	}
 
 	public function signup() {
+		if($this->request->is('post')){
+			$this->Couple->save($this->request->data);
+		}
+	}
+	public function editmydate($date_id)//coupleから飛んでくる？
+	{
+		$this->Date->id = $date_id;
+    if ($this->request->is('post') || $this->request->is('put')) {
+      $this->Date->save($this->Date->data);
+      $this->redirect(array('action' => 'couple'));
+    } else {
+      $this->request->data = 
+          $this->Date->read(null, $date_id);
+    }
 		
+	}
+	public function deleatemydate($date_id)
+	{
+		$this->date->deleate($date_id);
 	}
 }
--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -1,21 +1,49 @@
 <?php 
-
+App::uses('CakeEmail', 'Network/Email');
 class UsersController extends AppController {
 	public $helper = array('HTML', 'form');
 
-	public function mypage() {
-
+	public function mypage($id) {
+		//user関数と機能が全く同じ。認証機能などをつけるべき？
+		$this->set('user',$this->User->getuser($id));
 	}
 
-	public function edit() {
+	public function edit($id) {
+	//フォームとして送られてくることを想定
+		$this->User->id=$id;
+		if($this->request->is('post')||$this->request->is('put')){
+			$this->User->save($this->request->data);
+	//		$this->User->redirect(array('action'=>'./mypage'));
+		}else{
+			$this->request->data=$this->User->read(null,$id);
+		}
 
 	}
 
 	public function signup() {
-		
+			if($this->request->is('post')){
+			$this->User->save($this->request->data);
+		}
 	}
 
-	public function user() {
-		
+	public function user($id) {
+		$this->set('user',$this->User->getuser($id));
+	}
+	public function invite($id)//途中
+	{
+	//諸所設定はhttp://qiita.com/kazu56/items/cd58366f5fb74881ae06を見て行う
+		$mail="";
+		$name=$this->User->getuser($id)->name;
+		if($this->request->is('post')){
+			$mail=$this->request->data->mail;
+
+		}
+		$email = new CakeEmail('default');
+		$email->from('example@example.com');
+		$email->to($mail);
+		$email->subject($name);
+		//メール送信する
+		$email->send('メール本文');
+		//url作成をする必要。
 	}
 }
